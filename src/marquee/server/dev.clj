37ef5d87bfd3152c1f4b6e11ecea1c87e692492b
niten <niten@fudo.org>
2026-06-03 17:20:27 -0700

    Fix api-request? to use HttpRequest Java class methods

--- src/marquee/server/dev.clj
+++ src/marquee/server/dev.clj
@@ -1,5 +1,7 @@
-(ns marquee.server.dev)
+(ns marquee.server.dev
+  (:require [clojure.string :as str])
+  (:import [shadow.http.server HttpRequest]))
 
 ;; Used by shadow-cljs :proxy-predicate to route only /api/* to the BFF.
-(defn api-request? [req _config]
-  (clojure.string/starts-with? (:uri req) "/api"))
+(defn api-request? [^HttpRequest req _config]
+  (str/starts-with? (.getRequestPath req) "/api"))
